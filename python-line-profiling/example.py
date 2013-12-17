27204421f6ad88c36458e148f1b89b33776d4323
Dan Riti <dmriti@gmail.com>
2013-12-17 21:38:13 +0000

    Add example.py file.

---
+++ python-line-profiling/example.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+from time import sleep
+
+@profile
+def so_slow(bar):
+    """ Simulate a slow function. """
+    sleep(5)
+    return bar
+
+@profile
+def its_time_for_the_calulator(foo):
+    """ It's time for the calculator. """
+    if not isinstance(foo, int):
+        return None
+
+    a = []
+    for i in xrange(foo):
+        a.append(i)
+
+    b = so_slow(a)
+
+    c = 0
+    for i in xrange(foo):
+        c += i
+
+    return None
+
+def main():
+    print its_time_for_the_calulator(100000)
+
+if __name__ == "__main__":
+    main()
